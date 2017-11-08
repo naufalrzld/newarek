90402fff8237c59ad272724bb4d70a35c30252d4
ramaadtym <ramaadtym@gmail.com>
2017-11-08 19:26:09 +0700

    Update arek

--- application/views/laman/v_login.php
+++ application/views/laman/v_login.php
@@ -3,11 +3,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
     <!-- Page Content -->
-    <div class="container">
+    <div class="container ayam">
         <div class="row">
-            <div class="col-lg-12">
-                <h1>Logo Nav by Start Bootstrap</h1>
-                <p>Note: You may need to adjust some CSS based on the size of your logo. The default logo size is 150x50 pixels.</p>
+            <div class="col-lg-7 gbr">
+
+            </div>
+            <div class="col-lg-5">
+                <nav>
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#">Registration Info</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li class="geserdikit"><a href="#">Sign Up</a></li>
+                    </ul>
+                </nav>
             </div>
         </div>
     </div>
