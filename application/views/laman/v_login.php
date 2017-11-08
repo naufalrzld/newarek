c8cdd19c9f79d276b76c2dd370a116743c7e80f5
ramaadtym <ramaadtym@gmail.com>
2017-11-08 16:52:01 +0700

    Update inisial project, penyempurnaan laman core

--- application/views/laman/v_login.php
+++ application/views/laman/v_login.php
@@ -2,3 +2,16 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
+    <!-- Page Content -->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1>Logo Nav by Start Bootstrap</h1>
+                <p>Note: You may need to adjust some CSS based on the size of your logo. The default logo size is 150x50 pixels.</p>
+            </div>
+        </div>
+    </div>
+    <!-- /.container -->
+
+    
+
