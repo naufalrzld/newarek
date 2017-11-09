bb56491508ca0f3598a1590ea619e6182d638e45
ramaadtym <ramaadtym@gmail.com>
2017-11-09 16:47:01 +0700

    Update new arek, laman login

--- application/views/laman/v_login.php
+++ application/views/laman/v_login.php
@@ -3,12 +3,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
     <!-- Page Content -->
-    <div class="container ayam">
-        <div class="row">
-            <div class="col-lg-7 gbr">
 
+        <div class="row ayam">
+            <div class="col-lg-7 gbr">
+                    <div class="logo"></div>
+                    <div class="tulisan">
+                        <p class="judul">Aplikasi Rekruitasi </p>
+                        <p class="motion">Mobile Innovation Laboratory</p>
+                        <p class="tahun"><?php echo date("Y")+1;?></p>
+                        <div class="lbr_grs grs">
+
+                        </div>
+                    </div>
             </div>
-            <div class="col-lg-5">
+            <div class="col-lg-5 formlogin">
                 <nav>
                     <ul class="nav navbar-nav">
                         <li class="active"><a href="#">Home</a></li>
@@ -17,9 +25,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         <li class="geserdikit"><a href="#">Sign Up</a></li>
                     </ul>
                 </nav>
+                <div class="row">
+                    <div class="col-lg-8 formulir">
+                        <p class="signin">Sign In</p>
+                        <div class="grs_sign"></div>
+                        <form>
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="email" placeholder="Username">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" id="pwd"  placeholder="Password">
+                            </div>
+                            <button type="submit" class="btn btn-danger btn-lg btn-block tombol-login">Sign In</button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
     <!-- /.container -->
 
     
