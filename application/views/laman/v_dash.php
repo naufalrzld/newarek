ec45b47d8d3a75a228d179b3caa2981ea771f45b
ramaadtym <faishalr97@gmail.com>
2018-02-02 16:11:08 +0700

    Perbaikan Dashboard sedikit

--- application/views/laman/v_dash.php
+++ application/views/laman/v_dash.php
@@ -17,18 +17,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 Selamat Datang, <span class="nama">Cahyadi</span>
             </p>
             <ul class="navbar-nav welcome">
-                <li class="nav-item dropdown">
-                    <a class="nav-link menu_link_txt dropdown-toggle" href="#" id="navbarDropdown"
-                       role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img src="<?php echo base_url();?>assets/img/user.svg" width="30" height="30" class="d-inline-block align-top" alt="">
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
+                <img src="<?php echo base_url();?>assets/img/user.svg" width="30" height="30" d-inline-block align-top" alt="">
+
             </ul>
         </div>
     </nav>
