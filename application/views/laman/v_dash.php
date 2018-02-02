56976613e402f7947a1f8c73b897c3c1eb3bac3e
ramaadtym <ramaadtym@gmail.com>
2018-02-02 18:27:54 +0700

    Update laman dashboard

--- application/views/laman/v_dash.php
+++ application/views/laman/v_dash.php
@@ -14,32 +14,74 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <!-- Menu Bar-->
         <div class="collapse navbar-collapse float-right" id="navbarSupportedContent">
             <p class="welcome_txt">
-                Selamat Datang, <span class="nama">Cahyadi</span>
+               Muhammad Adi Bayu Setiawan
+                <span class="last_log">Login Terakhir: <?php echo date('D, d/m/Y');?></span>
             </p>
             <ul class="navbar-nav welcome">
-                <img src="<?php echo base_url();?>assets/img/user.svg" width="30" height="30" d-inline-block align-top" alt="">
+                <img src="<?php echo base_url();?>assets/img/user.svg" width="50" height="50" d-inline-block align-top img-fluid" alt="">
 
             </ul>
         </div>
     </nav>
 </nav>
-
+<!-- Nav bawah Header-->
+<nav class="navbar menu navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <ul class="navbar-nav jarak">
+            <li class="nav-item active">
+                <a class="navbar-brand" href="#"><div class="home"></div> </a>
+            </li>
+            <li class="nav-item dropdown jarak">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Informasi Rekrutasi
+                </a>
+                <div class="dropdown-menu utama" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown jarak">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Registrasi Calon Anggota
+                </a>
+                <div class="dropdown-menu utama" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown jarak">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Tes dan Wawancara
+                </a>
+                <div class="dropdown-menu utama" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown jarak">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Pengumuman
+                </a>
+                <div class="dropdown-menu utama" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
+<!--End of Nav Bawah header-->
 
 <div class="container-fluid">
-    <div class="row">
-        <div class="col-lg-12">
-            <nav aria-label="breadcrumb">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="#">Home</a></li>
-                </ol>
-            </nav>
-        </div>
-    </div>
     <div class="row">
         <div class="col-lg-1 sesi">
             <div class="nomor">1</div>
             <div class="judul_sesi">
-                Pengisian Data Diri
+                Biodata Diri
             </div>
         </div>
         <div class="col-lg-8">
